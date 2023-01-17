6124290e09e21ea6ad36df0adba822f8a145ceaf
Rupkumar Sarkar <109466063+rup52333@users.noreply.github.com>
2023-01-17 13:37:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your code here
+var text = document.getElementById("evaluatedText").value;
+var numWords = 0;
+          
+            // Loop through the text
+            // and count spaces in it 
+            for (var i = 0; i < text.length; i++) {
+                var currentCharacter = text[i];
+          
+                // Check if the character is a space
+                if (currentCharacter == " ") {
+                    numWords += 1;
+                }
+            }
+numWords += 1;
+          
+            // Display it as output
+            document.getElementById("letterCount")
+                .innerHTML = numWords;
